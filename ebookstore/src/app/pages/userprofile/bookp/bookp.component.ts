6dabca8d4c8b81ce38044a7b7c4391139ba0de8a
dinela <dinelaganovic99@gmail.com>
2023-08-29 16:55:23 +0200

    edits

--- ebookstore/src/app/pages/userprofile/bookp/bookp.component.ts
+++ ebookstore/src/app/pages/userprofile/bookp/bookp.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { BookService } from 'src/app/services/book.service';
 @Component({
   selector: 'app-bookp',
   templateUrl: './bookp.component.html',
   styleUrls: ['./bookp.component.css']
 })
-export class BookpComponent {
+export class BookpComponent implements OnInit{
+  getBookId:any;
+  bookData:any;
+  bookList$!:Observable<any[]>;
+  autorList$!:Observable<any[]>;
+  izdavacList$!:Observable<any[]>;
+constructor( private param: ActivatedRoute, private api: BookService) {
+  this.getBookId=this.param.snapshot.paramMap.get('id');
+  this.api.getBookInfo(this.getBookId).subscribe((data) => {
+    this.bookData = data;
+  });
+
+}
+ngOnInit(): void {
+  this.bookList$= this.api.getBookList();
+  this.autorList$= this.api.getAutorList();
+  this.izdavacList$=this.api.getIzdavacList();
+}
 
 }
